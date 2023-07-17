258a45a72ff0c469b24b03d1b3ee10c1a651fba2
mariia.onuschuk <marya.vadymovna@gmail.com>
2023-07-18 00:41:56 +0300

    ...

--- src/composables/createCRMtask.ts
+++ src/composables/createCRMtask.ts
@@ -73,6 +73,7 @@ export default async function(orderNumber: string) {
 			'Content-Type': 'application/json',
 			'Accept': 'application/json',
 			'Cache-Control': 'no-cache',
+			'mode': 'no-cors',
 			'Pragma': 'no-cache',
 			'Authorization': `Bearer ${config.public.crmkey}`,
 		},
