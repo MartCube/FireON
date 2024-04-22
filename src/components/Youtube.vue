e358a7827e0a6f6261e33bfd3a1d59feac87fda6
mart cube <martingjorceski@gmail.com>
2024-04-22 12:29:33 +0200

    youtube component with image

--- src/components/Youtube.vue
+++ src/components/Youtube.vue
@@ -1,6 +1,19 @@
 <template>
 	<div class="youtube">
-		<iframe :src="src"  frameborder="0" allowfullscreen></iframe>
+		<div
+			v-if="!showFrame"	
+			class="image" 
+			@click="showFrame=true"
+		>
+			<img :src="img"  lazyloadloading="lazy" />
+			<Icon name="IconYoutube" />	
+		</div>
+		<iframe 
+			v-if="showFrame" 
+			:src="src" 
+			frameborder="0" 
+			loading="lazy"
+		/>
 	</div>
 </template>
 
@@ -10,13 +23,36 @@ const props = defineProps<{
 }>()
 // http://www.get-youtube-thumbnail.com/
 const src = `https://www.youtube.com/embed/${props.id}`
+const img = `https://i3.ytimg.com/vi/${props.id}/maxresdefault.jpg`
+const showFrame = ref(false)
 </script>
 
 <style lang="scss" scoped>
 .youtube {
+	margin-top: 2rem;
 	width: 100%;
-	height: 300px;
-	position: relative;
+	max-width: 500px;
+	height: 280px;
+
+	.image{
+		position: relative;
+		img{
+			width: 100%;
+			height: 100%;
+			filter: brightness(50%);
+			cursor: pointer;
+		}
+		.icon {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%,-50%);
+			width: 3.5rem;
+			height: 3.5rem;
+			stroke: $primary;
+			fill: none;
+		}
+	}
 	iframe {
 		width: 100%;
 		height: 100%;
