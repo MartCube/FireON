4b6304dc71da576e8f02a29ef973eb03c1732043
Maryanushka <marya.vadymovna@gmail.com>
2024-12-14 21:09:24 +0200

    package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,123 +1,123 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
+ srcDir: 'src',
+ css: ["@/assets/css/app.scss"],
 
-	srcDir: 'src',
-
-	css: ["@/assets/css/app.scss"],
-
-	typescript: {
-		strict: true,
-		typeCheck: false,
-		shim: false
+ typescript: {
+					strict: true,
+					typeCheck: false,
+					shim: false
 	},
 
-	components: {
-		"dirs": [
-			{ "path": "~/components", "global": true },
-		],
+ components: {
+					"dirs": [
+									{ "path": "~/components", "global": true },
+					],
 	},
 
-	app: {
-		pageTransition: { name: 'page', mode: 'out-in' },
-		head: {
-			link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
-		}
+ app: {
+					pageTransition: { name: 'page', mode: 'out-in' },
+					head: {
+									link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+					}
 	},
 
-	modules: [
-		'@nuxtjs/sanity',
-		'@pinia/nuxt',
-		'@nuxtjs/i18n',
-		'nuxt-icon',
-		// '@nuxtjs/google-analytics',
-		'nuxt-gtag',
-		// '~/modules/purge-comments'
-		// '~/modules/style-inlining',
+ modules: [
+					'@nuxtjs/sanity',
+					'@pinia/nuxt',
+					'@nuxtjs/i18n',
+					'nuxt-icon',
+					// '@nuxtjs/google-analytics',
+					'nuxt-gtag',
+					// '~/modules/purge-comments'
+					// '~/modules/style-inlining',
 	],
 
-	i18n: {
-		locales: [
-			{
-				code: 'ua',
-				name: 'Ukrainian',
-			},
-			{
-				code: 'en',
-				name: 'English',
+ i18n: {
+					locales: [
+									{
+													code: 'ua',
+													name: 'Ukrainian',
+									},
+									{
+													code: 'en',
+													name: 'English',
+									}
+					],
+					defaultLocale: 'ua',
+					strategy: 'prefix_except_default',
+					// lazy: true,
+					// langDir: 'locales/',
+					vueI18n: {
+									legacy: false,
+									locale: 'ua',
+									fallbackLocale: 'ua',
+									availableLocales: ['ua', 'en',],
+									messages: {
+					en: {
+							toHome: 'To Home',
+																	emailCopied: 'email copied',
+																	callme: 'Call me',
+																	iban: 'Iban',
+																	payment: 'Payment',
+					},
+					ua: {
+							toHome: 'На головну',
+																	emailCopied: 'адресу скопійовано',
+																	callme: 'Передзвоніть мені',
+																	iban: 'Сплата по IBAN',
+																	payment: 'Накладений платіж',
+					},
 			}
-		],
-		defaultLocale: 'ua',
-		strategy: 'prefix_except_default',
-		// lazy: true,
-		// langDir: 'locales/',
-		vueI18n: {
-			legacy: false,
-			locale: 'ua',
-			fallbackLocale: 'ua',
-			availableLocales: ['ua', 'en',],
-			messages: {
-        en: {
-          toHome: 'To Home',
-					emailCopied: 'email copied',
-					callme: 'Call me',
-					iban: 'Iban',
-					payment: 'Payment',
-        },
-        ua: {
-          toHome: 'На головну',
-					emailCopied: 'адресу скопійовано',
-					callme: 'Передзвоніть мені',
-					iban: 'Сплата по IBAN',
-					payment: 'Накладений платіж',
-        },
-      }
-		}
+					}
 	},
 
-	sanity: {
-		projectId: 'okruw9dl',
-		dataset: 'production',
-		minimal: true,
-		apiVersion: '2023-01-01'
+ sanity: {
+					projectId: 'okruw9dl',
+					dataset: 'production',
+					minimal: true,
+					apiVersion: '2023-01-01'
 	},
 
-	nitro: {
-		prerender: {
-			crawlLinks: true,
-			routes: ['/sitemap.xml']
-		}
+ nitro: {
+					prerender: {
+									crawlLinks: true,
+									routes: ['/sitemap.xml']
+					}
 	},
 
-	runtimeConfig: {
-		public: {
-			mono: process.env.NUXT_MONO, // can be overridden by NUXT_API_SECRET environment variable
-			novaposhta: process.env.NUXT_NOVAPOSHTA,
-			npEndpoint: 'https://api.novaposhta.ua/v2.0/json/',
-			monoEnpoint: 'https://api.monobank.ua/api/merchant/invoice/',
-			sendGrid: process.env.NUXT_SENDGRID_API_KEY,
-			crmkey: process.env.NUXT_CRM,
-			crmEndpoint: 'https://api.keepincrm.com/v1/',
-			domain: process.env.NUXT_DOMAIN,
-			gtagId: 'G-WN7B494RN5',
-    }
-  },
+ runtimeConfig: {
+					public: {
+									mono: process.env.NUXT_MONO, // can be overridden by NUXT_API_SECRET environment variable
+									novaposhta: process.env.NUXT_NOVAPOSHTA,
+									npEndpoint: 'https://api.novaposhta.ua/v2.0/json/',
+									monoEnpoint: 'https://api.monobank.ua/api/merchant/invoice/',
+									sendGrid: process.env.NUXT_SENDGRID_API_KEY,
+									crmkey: process.env.NUXT_CRM,
+									crmEndpoint: 'https://api.keepincrm.com/v1/',
+									domain: process.env.NUXT_DOMAIN,
+									gtagId: 'G-WN7B494RN5',
+	}
+},
 
-	vite: {
-		logLevel: 'info',
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: '@import "@/assets/css/colors.scss";',
-				},
-			},
-		},
+ vite: {
+					logLevel: 'info',
+					css: {
+									preprocessorOptions: {
+													scss: {
+																	additionalData: '@import "@/assets/css/colors.scss";',
+													},
+									},
+					},
 	},
 
+ // googleAnalytics: {
+	//   id: 'GTM-K49KX72B'
+	// }
 	gtag: {
-		id: 'G-WN7B494RN5',
+					id: 'G-WN7B494RN5',
 	},
-	// googleAnalytics: {
-  //   id: 'GTM-K49KX72B'
-  // }
+
+ compatibilityDate: '2024-12-14',
 })
